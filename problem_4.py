e52740482f1b8a68def5e685a18307cf4a2741dd
Graziele99 <142513653+Graziele99@users.noreply.github.com>
2023-08-25 11:42:56 -0300

    Update problem_4.py

--- problem_4.py
+++ problem_4.py
@@ -28,7 +28,46 @@ Você digitou 3 valores pares.
 
 
 def main():
-    pass  # your code here
+    # -*- coding: utf-8 -*-
+
+# YOUR FULL NAME
+# UAG00098
+# Problem Set 2 - Problem 3
+# Description:
+
+"""
+Inputs, Processes and Output (IPO)
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+Input(s):
+Digite 6 valores inteiros quaisquer.
+Exemplo:
+Digite o valor 1/6: 2
+Digite o valor 2/6: 3
+Digite o valor 3/6: 4
+Digite o valor 4/6: 5
+Digite o valor 5/6: 6
+Digite o valor 6/6: 7
+
+Processes:
+Faça um programa que leia 6 valores inteiros. Conte quantos destes valores digitados são pares e mostre esta informação.
+
+Output(s):
+Exemplo:
+Você digitou 3 valores pares.
+"""
+
+
+def main():
+     def main():
+    num_pares = 0
+
+    for i in range(6):
+        valor = int(input(f"Digite o valor {i+1}/6: "))
+    
+        if valor % 2 == 0:
+            num_pares += 1
+
+    print(f"Você digitou {num_pares} valores pares.")
 
 
 if __name__ == '__main__':
