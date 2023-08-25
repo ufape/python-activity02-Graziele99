4591d10fd460edd1c20f08e9d86376d07cf3e52f
Graziele99 <142513653+Graziele99@users.noreply.github.com>
2023-08-25 14:07:46 -0300

    Update problem_4.py

--- problem_4.py
+++ problem_4.py
@@ -2,7 +2,7 @@
 
 # YOUR FULL NAME
 # UAG00098
-# Problem Set 2 - Problem 1
+# Problem Set 2 - Problem 3
 # Description:
 
 """
@@ -28,16 +28,16 @@ Você digitou 3 valores pares.
 
 
 def main():
-   a = int(input(f"Digite o valor A: "))
-   b = int(input(f"Digite o valor B: "))
-   c = int(input(f"Digite o valor C: "))
-   d = int(input(f"Digite o valor D: "))
+    num_pares = 0
 
-   if b > c and d > a and (c + d) > (a + b) and c > 0 and d > 0 and a %2 == 0:
-     print("Valores aceitos.")
-   else:
-     print("Valores recusados.")
-     
+    for i in range(6):
+        valor = int(input(f"Digite o valor {i+1}/6: "))
+    
+        if valor % 2 == 0:
+            num_pares += 1
+
+    print(f"Você digitou {num_pares} valores pares.")
+
+
 if __name__ == '__main__':
     main()
-    
