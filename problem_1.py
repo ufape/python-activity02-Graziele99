8427b58e51e44a42b42a038c444f600d1a4f6757
Graziele99 <142513653+Graziele99@users.noreply.github.com>
2023-08-25 12:06:47 -0300

    Update problem_1.py

--- problem_1.py
+++ problem_1.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 
 # YOUR FULL NAME
 # UAG00098
@@ -9,67 +9,21 @@
 Inputs, Processes and Output (IPO)
 -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 Input(s):
-Quatro números inteiros v1, v2, v3 e v4.
-Exemplo 1:
-Digite o valor A: 5
-Digite o valor B: 6
-Digite o valor C: 7
-Digite o valor D: 8
-Exemplo 2:
-Digite o valor A: 2
-Digite o valor B: 3
-Digite o valor C: 2
-Digite o valor D: 6
+Digite 6 valores inteiros quaisquer.
+Exemplo:
+Digite o valor 1/6: 2
+Digite o valor 2/6: 3
+Digite o valor 3/6: 4
+Digite o valor 4/6: 5
+Digite o valor 5/6: 6
+Digite o valor 6/6: 7
 
 Processes:
-Leia 4 valores inteiros A, B, C e D. A seguir, se B for maior do que C e se D for maior do que A, e a soma de C com D for maior que a soma de A e B e se C e D, ambos, forem positivos e se a variável A for par escrever a mensagem "Valores aceitos.", senão escrever "Valores recusados.".
+Faça um programa que leia 6 valores inteiros. Conte quantos destes valores digitados são pares e mostre esta informação.
 
 Output(s):
-Mostre a respectiva mensagem após a validação dos valores.
-Exemplo 1:
-Valores recusados.
-Exemplo 2:
-Valores aceitos.
-
-
-"""
-
-
-def main():
-    # -*- coding: utf-8 -*-
-
-# YOUR FULL NAME
-# UAG00098
-# Problem Set 2 - Problem 1
-# Description:
-
-"""
-Inputs, Processes and Output (IPO)
--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-Input(s):
-Quatro números inteiros v1, v2, v3 e v4.
-Exemplo 1:
-Digite o valor A: 5
-Digite o valor B: 6
-Digite o valor C: 7
-Digite o valor D: 8
-Exemplo 2:
-Digite o valor A: 2
-Digite o valor B: 3
-Digite o valor C: 2
-Digite o valor D: 6
-
-Processes:
-Leia 4 valores inteiros A, B, C e D. A seguir, se B for maior do que C e se D for maior do que A, e a soma de C com D for maior que a soma de A e B e se C e D, ambos, forem positivos e se a variável A for par escrever a mensagem "Valores aceitos.", senão escrever "Valores recusados.".
-
-Output(s):
-Mostre a respectiva mensagem após a validação dos valores.
-Exemplo 1:
-Valores recusados.
-Exemplo 2:
-Valores aceitos.
-
-
+Exemplo:
+Você digitou 3 valores pares.
 """
 
 
@@ -83,11 +37,8 @@ def main():
      print("Valores aceitos.")
    else:
      print("Valores recusados.")
-
 if __name__ == '__main__':
     main()
+    
 
-
-
-if __name__ == '__main__':
-    main()
+    
